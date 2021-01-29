8b95d4c2c429461d0419970d03fc7e70da8ca3f7
Matthew Courtney <matthewdc182@gmail.com>
2021-01-29 17:09:42 -0500

    added in the view by department function was a little tricky but i also figured out how to rename columns

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { connect } = require("http2");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,47 +17,96 @@ connection.connect(function (err) {
 });
 
 function initiateApp() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee Role", "Update employee manager", "View all roles", "View all departments"],
-            name: "initial_choice"
-        }
-    ]).then(function(res) {
-        var response = res.initial_choice;
-        if(response === "View all employees") {
-            displayAllEmployees();
-        }
-        else if (response === "View all roles") {
-            displayAllRoles();
-        }
-        else if(response === "View all departments") {
-            displayAllDepartments();
-        }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "View all employees by department",
+          "View all employees by manager",
+          "Add employee",
+          "Remove employee",
+          "Update employee Role",
+          "Update employee manager",
+          "View all roles",
+          "View all departments",
+        ],
+        name: "initial_choice",
+      },
+    ])
+    .then(function (res) {
+      var response = res.initial_choice;
+      if (response === "View all employees") {
+        displayAllEmployees();
+      } else if (response === "View all roles") {
+        displayAllRoles();
+      } else if (response === "View all departments") {
+        displayAllDepartments();
+      } else if (response === "View all employees by department") {
+        byDepartment();
+      }
     });
 
-    function displayAllEmployees() {
-        connection.query("SELECT * FROM employee", function(err,res) {
-            if(err) throw err;
-            console.table(res);
-            initiateApp();
-        })
-    };
-    
-    function displayAllRoles() {
-        connection.query("SELECT * FROM role", function(err,res) {
-            if(err) throw err;
-            console.table(res);
-            initiateApp();
-        })
-    };
+  // View All calls
 
-    function displayAllDepartments() {
-        connection.query("SELECT * FROM department", function(err,res) {
-            if(err) throw err;
-            console.table(res);
-            initiateApp();
-        })
-    };
+  function displayAllEmployees() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      initiateApp();
+    });
+  }
+
+  function displayAllRoles() {
+    connection.query("SELECT * FROM role", function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      initiateApp();
+    });
+  }
+
+  function displayAllDepartments() {
+    connection.query("SELECT * FROM department", function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      initiateApp();
+    });
+  }
+
+  // View By calls
+
+  function byDepartment() {
+    var dept;
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What Department?",
+          choices: ["HR", "Executive", "Management", "Associate"],
+          name: "department",
+        },
+      ])
+      .then(function (response) {
+        if (response.department === "HR") {
+          dept = 1;
+        } else if (response.department === "Executive") {
+          dept = 2;
+        } else if (response.department === "Management") {
+          dept = 3;
+        } else if (response.department === "Associate") {
+          dept = 4;
+        };
+        
+        connection.query(
+          "SELECT employee.first_name, employee.last_name, role.title, department.name AS department FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id WHERE department.id = " + dept, function(err,res) {
+              if(err) throw err;
+              console.table(res);
+          }
+        );
+      });
+  }
+
+  function byManager() {}
 }
