4d7d52e0452bc1401e299be907511029f5c4bb01
Matthew Courtney <matthewdc182@gmail.com>
2021-01-29 01:47:04 -0500

    I realized the join is not working the same in the function as it is in sqlWB need to figure out why

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connection.connect(function (err) {
 
 function afterConnection() {
   connection.query(
-    "SELECT e.id, e.first_name, e.last_name, title, salary, d.name, em.first_name, em.last_name FROM employee as e INNER JOIN role ON e.role_id = role.id INNER JOIN employee as em ON e.manager_id = em.id LEFT JOIN department AS d ON d.id = role.department_id;",
+    "SELECT * FROM employee",
     function (err, res) {
       if (err) throw err;
       console.table(res);
